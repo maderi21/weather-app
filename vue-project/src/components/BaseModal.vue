26c5f6bb44c2ffd616803f4291aa5f4e9055b347
xkostolanskym <kostolansky.marek@gmail.com>
2024-01-08 23:40:50 +0100

    BaseModal active

--- vue-project/src/components/BaseModal.vue
+++ vue-project/src/components/BaseModal.vue
@@ -1,8 +1,12 @@
 <template>
   <div
+    v-show="modalActive"
     class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
   >
-    <div class="p-4 bg-white self-start mt-32 max-w-screen-md">
+    <div
+      v-if="modalActive"
+      class="p-4 bg-white self-start mt-32 max-w-screen-md"
+    >
       <slot></slot>
       <button class="text-white mt-8 bg-weather-primary py-2 px-6">
         Close
@@ -11,6 +15,11 @@
   </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+defineProps({
+  modalActive: {
+    type: Boolean,
+    default: false,
+  },
+});
 </script>
