e597ee39b248139d8253190d0af57e733605f59b
xkostolanskym <kostolansky.marek@gmail.com>
2024-01-09 19:44:14 +0100

    HomeView.vue

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,5 +1,18 @@
-<script setup></script>
-
 <template>
-  <main></main>
+  <main class="container text-white">
+    <div class="pt-4 mb-4 relative">
+      <input
+        type="text"
+        v-model="searchQuery"
+        placeholder="Search for a city"
+        class="py-2 px-1 bg-transparent border-b focus:border-weather-secondary focus:outline-none"
+      />
+    </div>
+  </main>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const searchQuery = ref("");
+</script>
