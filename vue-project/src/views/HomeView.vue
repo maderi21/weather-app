a5ebbef28e53c6c53e8c10e0506420c949f7673b
xkostolanskym <kostolansky.marek@gmail.com>
2024-01-16 22:50:03 +0100

    search

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -8,6 +8,18 @@
         placeholder="Search for a city"
         class="py-2 px-1 bg-transparent border-b focus:border-weather-secondary focus:outline-none"
       />
+      <ul
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults"
+      >
+        <li
+          v-for="searchResult in mapboxSearchResults"
+          :key="searchResult.id"
+          class="py-2 cursor-pointer"
+        >
+          {{ searchResult.place_name }}
+        </li>
+      </ul>
     </div>
   </main>
 </template>
@@ -27,9 +39,10 @@ const getSearchResults = () => {
   queryTimeout.value = setTimeout(async () => {
     if (queryTimeout.value !== "") {
       const result = await axios.get(
-        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}`
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}$types=place`
       );
       mapboxSearchResults.value = result.data.features;
+
       return;
     }
     mapboxSearchResults.value = null;
