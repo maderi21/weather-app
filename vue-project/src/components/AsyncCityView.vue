61ac8727a7996b0df0d4e563e37ef712ee4edf39
xkostolanskym <kostolansky.marek@gmail.com>
2024-01-28 21:03:56 +0100

    weekly done

--- vue-project/src/components/AsyncCityView.vue
+++ vue-project/src/components/AsyncCityView.vue
@@ -71,6 +71,34 @@
       </div>
     </div>
     <hr class="border-white border-opacity-10 border w-full" />
+    <!-- Weekly weather -->
+    <div class="max-w-screen-md w-ful py-12">
+      <div class="mx-8 text-white">
+        <h2 class="mb-4">7 Day Forecast</h2>
+        <div
+          v-for="day in weatherData.daily"
+          :key="day.dt"
+          class="flex items-center"
+        >
+          <p class="flex-1">
+            {{
+              new Date(day.dt * 1000).toLocaleDateString("en-us", {
+                weekday: "long",
+              })
+            }}
+          </p>
+          <img
+            class="w-[50px] h-[50px] objet-cover"
+            :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`"
+            alt=""
+          />
+          <div class="flex gap-2 flex-1 justify-end">
+            <p>H: {{ Math.round(day.temp.max) }}</p>
+            <p>L: {{ Math.round(day.temp.min) }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
